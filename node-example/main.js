ac8fee76d70e51f0b98797cbfbfa093bb62ef0dd
luoxinzheng <luoxinzheng@fagougou.com>
2021-08-04 10:27:10 +0800

    doc docx 转换为 text 实验

---
+++ node-example/main.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const pdf = require('pdf-parse')
+const { getText } = require('any-text')
+const WordExtractor = require('word-extractor')
+const extractText = require('office-text-extractor')
+const fileType = require('file-type')
+// const pdf2txt = require('pdf2txt')
+
+const filename = '/Users/luoxinzheng/response.doc';
+const docxfile = '/Users/luoxinzheng/response.docx';
+const pdffile = '/Users/luoxinzheng/劳动合同.pdf';
+
+
+(async() => {
+    // // const text = await getText(filename)
+    // const doc = await (new WordExtractor()).extract(filename)
+    // console.log(doc.getBody())
+    // const buf = fs.readFileSync(pdffile)
+    // const data = await pdf(buf)
+    // console.log(data.text)
+    let text = await extractText(docxfile)
+    text = text.replace(/[\s]/g, '')
+    console.log(text)
+    // console.log(JSON.stringify(text))
+    // console.log(await fileType.fromFile(filename))
+    // console.log(await fileType.fromFile(docxfile))
+    // console.log(await fileType.fromFile(pdffile))
+    // const output = await pdf2txt.read(pdffile)
+    // console.log(output)
+})()
---
+++ node-example/readme.md
@@ -0,0 +1,13 @@
+
+## 文件MIME类型
+WPS doc
+{ ext: 'cfb', mime: 'application/x-cfb' }
+MS-DOC
+
+docx
+{
+  ext: 'docx',
+  mime: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+}
+pdf
+{ ext: 'pdf', mime: 'application/pdf' }
