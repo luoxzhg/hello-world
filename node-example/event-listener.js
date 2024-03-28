87c47fa7455b8b8f85b2bf97feb2f5a5a0766088
星星 <luoxinzheng@fagougou.com>
2024-03-28 13:40:13 +0800

    chore

--- node-example/event-listener.js
+++ node-example/event-listener.js
@@ -6,6 +6,8 @@ function listener() {
    console.log('listener started')
    setImmediate(() => console.log('nested listener started'))
 }
+console.log('before newListener')
+emitter.on('e', listener)
 
 emitter.on('newListener', () => {
    console.log('new listener')
@@ -15,7 +17,8 @@ console.log('after newListener')
 emitter.on('e', listener)
 
 // emit 同步发布事件，并且按次序同步执行监听器
+console.log('before emitted event: e')
 emitter.emit('e')
-console.log('emitted event: e')
+console.log('after emitted event: e')
 
 // emitter.emit('error', new Error('whoops!'));
