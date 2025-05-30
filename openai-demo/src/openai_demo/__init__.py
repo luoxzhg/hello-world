43aa4e340d2b9a4ff896f288cbe6bd33c0397718
星星 <luoxinzheng@fagougou.com>
2025-05-30 11:34:23 +0800

    a

--- openai-demo/src/openai_demo/__init__.py
+++ openai-demo/src/openai_demo/__init__.py
@@ -1,6 +1,11 @@
 from os import getenv
 from pprint import pprint
 from openai import OpenAI
+
+# base64
+# b'c2stN2dDajlzRzFMTEZMdUNSbHBlN1dUM0JsYmtGSmtpUEhzY3BvVzdzR0ZSNjJlWml2'
+# b'c2stTE9Pcnp1bno5YzcwWnFlR0IwZDRUM0JsYmtGSlNYcTNoZHRzb0t0R1ZjdHVEeEJL'
+
 client = OpenAI(api_key=getenv('OPENAI_API_KEY'))
 
 completion = client.chat.completions.create(
